82e9e345b91b7d0dc81fa871fae7beb7f8597c56
Tim Cuthbertson <tim@gfxmonk.net>
2016-11-05 15:37:20 +1100

    port taglib -> mutagen; 0install -> nix

--- Makefile
+++
@@ -1,4 +0,0 @@
-0:
-	mkzero-gfxmonk -v date music-import.xml
-
-.PHONY: 0
---
+++ nix/default.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+with stdenv;
+{ src }:
+mkDerivation {
+	name = "music-import";
+	inherit src;
+	buildInputs = let py = python2Packages; in [
+		makeWrapper
+		mp3gain
+		py.readline py.mutagen
+		py.python py.pygtk py.notify
+	];
+	buildPhase = "true";
+	installPhase = ''
+		mkdir -p "$out/bin"
+		cp "music-import" "$out/bin"
+		wrapProgram "$out/bin/music-import" \
+			--prefix PATH : "${mp3gain}/bin" \
+			--set PYTHONPATH "$PYTHONPATH" \
+			;
+	'';
+}
+
